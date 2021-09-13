6e62d75041215575a01b0f9ec355066d0342f17c
Ajmain Nishu <ajmainnishu@gmail.com>
2021-09-13 13:13:10 +0600

    test: Unwanted many files and classes deleted

--- js/app.js
+++ js/app.js
@@ -229,24 +229,26 @@ const showProducts = (products) => {
     const allProducts = products.map((pd) => pd);
     for (const product of allProducts) {
         const div = document.createElement("div");
-        div.classList.add("product");
-        div.innerHTML = `<div class="single-product">
-        <div>
-        <img class="product-image" src=${product.image}></img>
+        // div.classList.add("product");
+        div.innerHTML = `
+        <div class="single-product">
+            <div>
+                <img class="product-image" src=${product.image}></img>
+            </div>
+            <h3>${product.title}</h3>
+            <p>Category: ${product.category}</p>
+            <p>Rating : ${product.rating.rate}</p>
+            <p>Reviews: ${product.rating.count}</p>
+            <h2>Price:  ${product.price}</h2>
+            <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="btn btn-secondary">add to cart</button>
+            <button id="details-btn" class="btn btn-outline-secondary">Details</button>
         </div>
-        <h3>${product.title}</h3>
-        <p>Category: ${product.category}</p>
-        <p>Rating : ${product.rating.rate}</p>
-        <p>Reviews: ${product.rating.count}</p>
-        <h2>Price: $ ${product.price}</h2>
-        <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-        <button id="details-btn" class="btn btn-danger">Details</button></div>
         `;
         document.getElementById("all-products").appendChild(div);
     }
 };
 let count = 0;
-const addToCart = (id, price) => {
+const addToCart = (price) => {
     count = count + 1;
     updatePrice("price", price);
 
