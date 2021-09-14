a02e92fc9dab1a83e85b72e75a5b5923cf36a196
Ajmain Nishu <ajmainnishu@gmail.com>
2021-09-14 14:31:17 +0600

    test: bug fixed in card price doller symbol

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ const showProducts = (products) => {
             <p>Category: ${product.category}</p>
             <p>Rating : ${product.rating.rate}</p>
             <p>Reviews: ${product.rating.count}</p>
-            <h2>Price:  ${product.price}</h2>
+            <h2>Price: $ ${product.price}</h2>
             <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-secondary">add to cart</button>
             <button id="details-btn" class="btn btn-outline-secondary">Details</button></div>
         `;
